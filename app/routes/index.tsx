ef3193a9d7cf76ee513e8942ee8e7063a7e51bd9
Burak <burak@90pixel.com>
2025-05-22 14:53:25 +0300

    Add initial project structure with configuration files and basic routing

---
+++ app/routes/index.tsx
@@ -0,0 +1,28 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { createServerFn } from "@tanstack/react-start";
+import { getCookie } from "@tanstack/react-start/server";
+
+const login = createServerFn({ method: "GET" }).handler(async () => {
+  // const headers = getHeaders();
+  // console.log({ headers });
+  const accessToken = getCookie("access_token");
+  console.log({ accessToken });
+  return "x";
+});
+
+export const Route = createFileRoute("/")({
+  component: Home,
+});
+
+function Home() {
+  return (
+    <button
+      type="button"
+      onClick={async () => {
+        const res = await login();
+        console.log(res);
+      }}>
+      Login and this will set httpOnly cookies
+    </button>
+  );
+}
