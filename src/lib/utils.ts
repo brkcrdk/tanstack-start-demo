ab1a1a3c38f8cfa0d170076570221d04085c83b2
Burak <burak@90pixel.com>
2025-06-16 21:19:09 +0300

    REFINE: Standardize import statements in utils.ts by changing double quotes to single quotes for consistency.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,5 @@
-import { cx } from "class-variance-authority";
-import { ClassNameValue, twMerge } from "tailwind-merge";
+import { cx } from 'class-variance-authority';
+import { ClassNameValue, twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassNameValue[]) {
   return twMerge(cx(inputs));
