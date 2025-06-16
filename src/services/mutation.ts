4b93a2def6a7f1bd80caf0eaf2d4e1477dae4434
Burak <burak@90pixel.com>
2025-06-16 17:55:49 +0300

    UPDATE: Enhance mutation service to include authentication handling; refactor interface and request logic for improved API interaction.

--- src/services/mutation.ts
+++ src/services/mutation.ts
@@ -1,11 +1,66 @@
+import { redirect } from '@tanstack/react-router';
+import { getCookie } from '@tanstack/react-start/server';
+
 import projectConfig from '@/app.config';
 
 const computedBaseUrl = `${projectConfig.baseUrl}/api`;
 
-interface MutationOptionsProps {
-  requireAuth: boolean;
+interface Props extends RequestInit {
+  requireAuth?: boolean;
 }
 
-async function mutation() {}
+async function mutation<T>(endpoint: string, requestInit?: Props) {
+  if (requestInit?.requireAuth) {
+    const accessToken = getCookie('access_token');
+    const refreshToken = getCookie('refresh_token');
+
+    if (!accessToken && !refreshToken) {
+      redirect({ to: '/logout' });
+    }
+
+    const computedRequestParams: RequestInit = {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+        ...requestInit?.headers,
+      },
+      ...requestInit,
+    };
+
+    if (requestInit?.body) {
+      computedRequestParams.body = requestInit.body;
+    }
+
+    const request = await fetch(`${computedBaseUrl}${endpoint}`, computedRequestParams);
+
+    if (!request.ok && request.status !== 401) {
+      const errorResponse = await request.json();
+
+      const { statusCode, error, message } = errorResponse;
+
+      return {
+        success: false,
+        error: {
+          statusCode,
+          error,
+          message,
+        },
+      };
+    }
+
+    if (!request.ok) {
+      throw new Error(`${request.status} ${request.statusText}`);
+    }
+
+    // const response: T = await request.json();
+    // return response;
+  } else {
+    const request = await fetch(`${computedBaseUrl}${endpoint}`, requestInit);
+
+    const response: T = await request.json();
+    return response;
+  }
+}
 
 export default mutation;
