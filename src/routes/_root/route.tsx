bac5528a5d6c29afe6528c8b0159bb15a8a7ae43
Burak <burak@90pixel.com>
2025-06-21 17:41:07 +0300

    REFINE: Update sign-up route to use new LoginForm import path, enhance root route with authentication token check and sidebar layout, and remove unused Header and Footer components for codebase cleanup.

--- src/routes/_root/route.tsx
+++ src/routes/_root/route.tsx
@@ -1,9 +1,57 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, Outlet, redirect } from '@tanstack/react-router';
+import { createServerFn } from '@tanstack/react-start';
+import { getCookie } from '@tanstack/react-start/server';
+
+import { AppSidebar } from '@/components/app-sidebar';
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '@/components/ui/breadcrumb';
+import { Separator } from '@/components/ui/separator';
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+
+export const chechkAuthToken = createServerFn({ method: 'GET' }).handler(async () => {
+  const accessToken = getCookie('access_token');
+  const refreshToken = getCookie('refresh_token');
+
+  if (!accessToken && !refreshToken) {
+    throw redirect({
+      to: '/login',
+    });
+  }
+});
 
 export const Route = createFileRoute('/_root')({
+  loader: () => chechkAuthToken(),
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/_root"!</div>;
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-[orientation=vertical]:h-4"
+            />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">Building Your Application</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
+  );
 }
--- src/routes/sign-up.tsx
+++ src/routes/sign-up.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
 
-import { LoginForm } from '@/components/login-form';
+import LoginForm from './login/components/LoginForm';
 
 export const Route = createFileRoute('/sign-up')({
   component: RouteComponent,
