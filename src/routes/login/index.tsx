7bd48b49be40480f64069040f53dc90671f10502
Burak <burak@90pixel.com>
2025-06-21 17:09:48 +0300

    REFINE: Update login route to include authentication token check and redirect logic, enhancing user session management.

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -1,22 +1,23 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
+import { createServerFn } from '@tanstack/react-start';
+import { getCookie } from '@tanstack/react-start/server';
 
 import LoginForm from './components/LoginForm';
 
-// import { LoginForm } from "@/components/login-form";
+export const chechkAuthToken = createServerFn({ method: 'GET' }).handler(async () => {
+  // Redirect the user to the home page
+  const accessToken = getCookie('access_token');
+  const refreshToken = getCookie('refresh_token');
 
-// export const chechkAuthToken = createServerFn({ method: "GET" }).handler(
-//   async () => {
-//     // Redirect the user to the home page
-//     const accessToken = getCookie("access_token");
-
-//     throw redirect({
-//       to: "/",
-//     });
-//   }
-// );
+  if (accessToken && refreshToken) {
+    throw redirect({
+      to: '/',
+    });
+  }
+});
 
 export const Route = createFileRoute('/login/')({
-  // loader: () => chechkAuthToken(),
+  loader: () => chechkAuthToken(),
   component: RouteComponent,
 });
 
