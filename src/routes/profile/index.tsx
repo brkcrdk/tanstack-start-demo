d7d9b9205d092d4d50a15df3781bc702f7f331f8
Burak <burak@90pixel.com>
2025-06-21 18:49:26 +0300

    ADD: Implement getCurrentUser service and update profile route to load current user data, enhancing RouteComponent to display user information.

--- src/routes/profile/index.tsx
+++ src/routes/profile/index.tsx
@@ -1,9 +1,20 @@
 import { createFileRoute } from '@tanstack/react-router';
 
+import getCurrentUser from '@/services/getCurrentUser';
+
+//LOOK AT THIS: https://tanstack.com/start/latest/docs/framework/react/examples/start-basic-react-query?path=examples%2Freact%2Fstart-basic-react-query%2Fsrc%2Froutes%2F__root.tsx
 export const Route = createFileRoute('/profile/')({
+  loader: () => getCurrentUser(),
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/profile/"! inner</div>;
+  const currentUser = Route.useLoaderData();
+
+  return (
+    <header>
+      <h1>Hello "/profile/"! inner</h1>
+      <pre>{JSON.stringify(currentUser, null, 4)}</pre>
+    </header>
+  );
 }
---
+++ src/services/getCurrentUser.ts
@@ -0,0 +1,30 @@
+import { createServerFn } from '@tanstack/react-start';
+
+import fetcher from '@/lib/fetcher';
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const getCurrentUser = createServerFn({
+  method: 'GET',
+}).handler(async () => {
+  await sleep(2000);
+  const response = await fetcher<User>({
+    url: '/current_user',
+    requireAuth: true,
+    formData: false,
+    fetchOptions: {
+      method: 'GET',
+    },
+  });
+  return response;
+});
+
+export default getCurrentUser;
