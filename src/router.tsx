3d2f7dc74ae7db6020557e7da643a913429bf7bd
Burak <burak@90pixel.com>
2025-07-06 15:58:18 +0300

    UPDATE: Add default preload delay of 300ms in createRouter function to enhance loading performance during navigation.

--- src/router.tsx
+++ src/router.tsx
@@ -14,6 +14,7 @@ export function createRouter() {
       routeTree,
       context: { queryClient },
       defaultPreload: 'intent',
+      defaultPreloadDelay: 300,
       defaultErrorComponent: DefaultCatchBoundary,
       defaultNotFoundComponent: () => <NotFound />,
     }),
