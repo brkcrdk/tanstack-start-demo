f590e4e2ee39bd05cd0824c9651ff27340eff2ac
Burak <burak@90pixel.com>
2025-07-06 15:06:39 +0300

    REFINE: Simplify profile form structure by extracting UserForm component, update profile schema validation, and enhance form handling with react-hook-form integration.

---
+++ src/routes/profile/components/UserForm.tsx
@@ -0,0 +1,112 @@
+import { Controller, useFormContext } from 'react-hook-form';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { cn } from '@/lib/utils';
+
+import { ProfileFormValues } from '../index';
+
+const localeOptions: Record<'tr' | 'en', string> = {
+  tr: 'Türkçe',
+  en: 'English',
+};
+
+function UserForm() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useFormContext<ProfileFormValues>();
+
+  const onSubmit = (data: ProfileFormValues) => {
+    console.log(data);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="mx-auto flex max-w-md flex-col gap-4"
+    >
+      <Label htmlFor="email">Email</Label>
+      <Controller
+        control={control}
+        name="email"
+        render={({ field }) => (
+          <Input
+            type="email"
+            id="email"
+            placeholder="test@test.com"
+            value={field.value}
+            readOnly
+            autoComplete="off"
+          />
+        )}
+      />
+      <Label htmlFor="firstName">First Name</Label>
+      <Controller
+        control={control}
+        name="firstName"
+        render={({ field }) => (
+          <Input
+            type="text"
+            id="firstName"
+            placeholder="test"
+            value={field.value}
+            onChange={field.onChange}
+            autoComplete="off"
+            className={cn(errors.firstName && 'border-red-500')}
+          />
+        )}
+      />
+      {errors.firstName && <span className="text-xs text-red-500">{errors.firstName.message}</span>}
+
+      <Label htmlFor="lastName">Last Name</Label>
+      <Controller
+        control={control}
+        name="lastName"
+        render={({ field }) => (
+          <Input
+            type="text"
+            id="lastName"
+            placeholder="test"
+            value={field.value}
+            autoComplete="off"
+            readOnly
+          />
+        )}
+      />
+      <Label htmlFor="locale">Locale</Label>
+      <Controller
+        control={control}
+        name="locale"
+        render={({ field }) => (
+          <Select
+            onValueChange={field.onChange}
+            value={field.value}
+          >
+            <SelectTrigger
+              id="locale"
+              className="w-full"
+            >
+              <SelectValue placeholder={localeOptions[field.value]} />
+            </SelectTrigger>
+            <SelectContent>
+              {Object.entries(localeOptions).map(([key, value]) => (
+                <SelectItem
+                  key={key}
+                  value={key}
+                >
+                  {value}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        )}
+      />
+      <Button type="submit">Kaydet</Button>
+    </form>
+  );
+}
+export default UserForm;
--- src/routes/profile/index.tsx
+++ src/routes/profile/index.tsx
@@ -1,32 +1,24 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useSuspenseQuery } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router';
-import { Controller, useForm, useFormState } from 'react-hook-form';
-import * as z from 'zod';
+import { FormProvider, useForm, useFormState } from 'react-hook-form';
+import * as z from 'zod/v4';
 
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import getCurrentUser from '@/services/getCurrentUser';
 
-const localeOptions: Record<'tr' | 'en', string> = {
-  tr: 'Türkçe',
-  en: 'English',
-};
+import UserForm from './components/UserForm';
 
 const profileSchema = z.object({
-  email: z.string().email(),
-  firstName: z.string(),
-  lastName: z.string(),
+  email: z.email(),
+  firstName: z.string().min(1, { message: 'First name is required' }),
+  lastName: z.string().min(1),
   locale: z.enum(['tr', 'en']),
 });
 
-type ProfileFormValues = z.infer<typeof profileSchema>;
+export type ProfileFormValues = z.infer<typeof profileSchema>;
 
 export const Route = createFileRoute('/profile/')({
   loader: async ({ context }) => {
-    context.queryClient.ensureQueryData({
+    context.queryClient.prefetchQuery({
       queryKey: ['currentUser'],
       queryFn: () => getCurrentUser(),
     });
@@ -50,74 +42,12 @@ function RouteComponent() {
     },
   });
 
-  const onSubmit = (data: ProfileFormValues) => {
-    console.log(data);
-  };
-
   const { isLoading } = useFormState({ control: form.control });
 
   return (
     <section aria-label="User Profile Area">
       <h4 className="text-2xl font-bold">Profile</h4>
-      {isLoading ? (
-        <div>Form loading...</div>
-      ) : (
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="mx-auto flex max-w-md flex-col gap-4"
-        >
-          <Label htmlFor="email">Email</Label>
-          {/* <Input
-          type="email"
-          id="email"
-          placeholder="test@test.com"
-          value={currentUser.data.email}
-          autoComplete="off"
-        />
-        <Label htmlFor="firstName">First Name</Label>
-        <Input
-          type="text"
-          id="firstName"
-          placeholder="test"
-          value={currentUser.data.firstName}
-          autoComplete="off"
-        />
-        <Label htmlFor="lastName">Last Name</Label>
-        <Input
-          type="text"
-          id="lastName"
-          placeholder="test"
-          value={currentUser.data.lastName}
-          autoComplete="off"
-        /> */}
-          <Label htmlFor="locale">Locale</Label>
-          <Controller
-            control={form.control}
-            name="locale"
-            render={({ field }) => (
-              <Select
-                onValueChange={field.onChange}
-                value={field.value}
-              >
-                <SelectTrigger id="locale">
-                  <SelectValue placeholder={localeOptions[field.value]} />
-                </SelectTrigger>
-                <SelectContent>
-                  {Object.entries(localeOptions).map(([key, value]) => (
-                    <SelectItem
-                      key={key}
-                      value={key}
-                    >
-                      {value}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-            )}
-          />
-          <Button type="submit">Kaydet</Button>
-        </form>
-      )}
+      <FormProvider {...form}>{isLoading ? <div>Form loading...</div> : <UserForm />}</FormProvider>
     </section>
   );
 }
